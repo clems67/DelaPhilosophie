d389ec02c98eb33ebf3abd3d521b9bc7ac6b3e23
clems67 <caravita.clement.67150@gmail.com>
2021-04-12 18:51:11 +0200

    first push

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+	include 'navbar.php'
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/styles/styles.css">
+	<title>De la Philosophie</title>
+</head>
+<body>
+
+	<object data="navbar.html"></object>
+
+	<div id="main">
+		<p class="introduction">De la Philosophie vout permet de découvrir 57 philosphes qui ont marqués l'histoire de la pensé. De Platon à Foucault, en passant par Descartes, Kant ou encore Nietzsche, vous pourrez vous familiariser avec la pensée des grands philosphes. Les meilleurs informations ont été choisies dans livres et sites internet pour rassembler le meilleur de chaque philosophe au même endroit, permettant ainsi une plus grande facilité de consultation. </p>
+		<br> <br>
+		<p style="text-align: center;"> Bonne navigation !</p>
+		<br> <br> <br>
+		<div id=liste_auteurs>
+			<strong>Liste des auteurs</strong> <br> <br> 
+			<div class="auteur"><a href="philosophes/platon.php">Platon (428 av.J.-C. - 348 av. J.-C.) </a></div> <br> 
+			<div class="auteur"><a href="philosophes/aristote.php">Aristote (348 av. J.-C. - 322 av. J.-C.) </a></div> <br> 
+		
+		</div>
+		<br> <br>
+		<div id=sources>
+			<strong>Sources</strong>
+			<br> <br>
+			- Dictionnaire de Philosophie, Bordas (Idées fondamentales, citations) <br>
+			- Philosophie Tle L-ES-S, Hatier (Programme) <br>
+			- Lire les philosophes, Hachette (L’auteur et son œuvre) <br>
+			- Wikipédia (Liste des œuvres + Vie et philosophie de certains auteurs) <br>
+			- sos.philosophie.free.fr/ (Philosophie de certains auteurs) <br>
+			- Cours-de-philosophie.fr (Vie des auteurs + philosophie de certains auteurs) <br>
+			- Les-philosophes.fr (Vie des auteurs) <br>
+			- atheisme.free.fr/Biographies/Lucrece.htm (Vie de Lucrèce) <br>
+			- jesusmarie.free.fr/anselme.html (Vie de Saint Anselme) <br>
+			- cosmovisions.com/Malebranche.htm (Vie de Malebranche) <br>
+			- onelittleangel.com/sagesse/citations/freud.asp (Vie de Freud) <br>
+			- babelio.com/auteur/Ludwig-Wittgenstein/3227 (Vie de Wittgenstein) <br>
+			- signosemio.com/wittgenstein/ (Vie de Wittgenstein, complément) <br>
+			- toupie.org/Biographies/Foucault.htm (Vie de Foucault) <br>
+			- babelio.com/auteur/Martin-Heidegger/4197 (Vie de Heidegger) <br>
+			- jesuismort.com/biographie_celebrite_chercher/biographie-martin_heidegger-5053.php (Vie de Heidegger, complément) <br>
+			- evene.lefigaro.fr/citations (Citations) <br>
+			- dicocitations.com/ (Citations) <br>
+			- atheisme.free.fr/Biographies/Occam.htm (Citations Ockham) <br>
+			- atheisme.free.fr/Biographies/Hobbes.htm (Citations Hobbes) <br>
+			- devoir-de-philosophie.com/citations/cournot.htm (Citations Cournot) <br>
+			- la-philosophie.com/citations-… (Citations Durkheim et Husserl) <br>
+			- wikiquote.org/wiki/ (Quelques citations) <br>
+			- augustecomte.org/spip.php?article41 (Œuvres de Comte) <br>
+			<br> <br>
+			Note concernant les oeuvres : les listes ne sont pas toujours exhaustives, et il se peut que certaines dates soient approximatives.
+		</div>
+		<br> <br>
+		<div id=credits style="text-align: center;">
+			<strong>Crédits</strong>
+			<br> <br>
+			Idées originales et documentation : Aurélia Vaudel <br>
+			Développement informatique : Clément CARAVITA
+			<br> <br>
+			Contactez-nous via philosophestles@gmail.com
+		</div>
+		<br> <br>
+	</div>
+	<!--<button type="button" onclick="document.getElementById('main').style.maxWidth='30%'">button</button> -->
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles/styles.css">
+	<title>De la Philosophie</title>
+</head>
+<body>
+
+	<nav class="navbar">
+		<div class="navbar_container">
+      <a id="navbar_logo" href="\De_la_Philosophie\index.php"> De la Philosophie
+      </a>
+      <div class="navbar_toggle" id="mobile-menu">
+          <span class="bar"></span> <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+			<ul class="navbar_menu">
+		    <li class="navbar_item">
+		      <a class="navbar_links" href="\De_la_Philosophie\index.php">Accueil</a>
+			  </li>        
+			</ul>
+		</div>
+	</nav>
+</html>
